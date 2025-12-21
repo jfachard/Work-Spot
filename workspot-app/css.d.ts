75237afcf72522062983760e2c73ccfa3ea51f0e
Achard Jean-Francis <jean-francis.achard@epitech.eu>
2025-12-21 23:41:17 +0100

    feat: add react-native-svg and react-native-svg-transformer dependencies

--- workspot-app/css.d.ts
+++ workspot-app/css.d.ts
@@ -2,3 +2,35 @@ declare module "*.css" {
   const content: any;
   export default content;
 }
+
+declare module '*.svg' {
+  import React from 'react';
+  import { SvgProps } from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: number;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: number;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: number;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: number;
+  export default content;
+}
+
+declare module '*.webp' {
+  const content: number;
+  export default content;
+}
--- workspot-app/metro.config.js
+++ workspot-app/metro.config.js
@@ -5,4 +5,16 @@ const { withNativewind } = require('nativewind/metro');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Configuration pour react-native-svg-transformer
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+};
+
+config.resolver = {
+  ...config.resolver,
+  assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...config.resolver.sourceExts, 'svg'],
+};
+
 module.exports = withNativewind(config);
